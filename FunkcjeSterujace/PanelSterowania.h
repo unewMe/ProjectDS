e33a80c8ca8cd4297bafd0306c6c16f83cecf929
Dawid Chudzicki <chudzickid@gmail.com>
2023-08-29 17:52:38 +0200

    -a

---
+++ FunkcjeSterujace/PanelSterowania.h
@@ -0,0 +1,14 @@
+//
+// Created by chudz on 29.08.2023.
+//
+
+#ifndef PROJECTDS_PANELSTEROWANIA_H
+#define PROJECTDS_PANELSTEROWANIA_H
+
+
+class PanelSterowania {
+
+};
+
+
+#endif //PROJECTDS_PANELSTEROWANIA_H
