0f288a767b80e5baa4044a6a0cd4053c93bda17d
Dawid Chudzicki <chudzickid@gmail.com>
2023-08-29 18:58:21 +0200

    -a

---
+++ Logika Biznesowa/Osoba.cpp
@@ -0,0 +1,19 @@
+#include<string>
+using namespace std;
+
+class Osoba
+{
+private:
+struct dataUrodzenia
+{
+    int dzien;
+    int miesiac;
+    int rok;
+};
+private:
+    string imie;
+    string nazwisko;
+    string mail;
+    dataUrodzenia dataUrodzenia;
+
+};
