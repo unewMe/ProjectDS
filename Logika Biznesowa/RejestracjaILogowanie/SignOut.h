fa3797450ecdfb16de87bb261f0028653ec658fb
Piotr Szczerbiak <noipanpiotr@wp.pl>
2023-08-30 13:31:55 +0200

    Wstepny szkielet rejestracja

--- Logika Biznesowa/RejestracjaILogowanie/SignOut.h
+++ Logika Biznesowa/RejestracjaILogowanie/SignOut.h
@@ -5,8 +5,18 @@
 #ifndef PROJECTDS_SIGNOUT_H
 #define PROJECTDS_SIGNOUT_H
 
+#include "../WirtualneByty/User.h"
+#include <string>
+
 
 class SignOut {
+public:
+    bool signout();
+    bool isEmailCorrect(string email);
+    bool isSexCorrect(string sex);
+    bool isDataCorrect(int day, int month, int year);
+    bool isCorrectRPassword(string password,string rPassword);
+
 
 };
 
