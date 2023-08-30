c71ed8470285d9c364ad60f085c97f386ea2b742
Dawid Chudzicki <chudzickid@gmail.com>
2023-08-30 14:09:03 +0200

    Poprawilem blad

--- Logika Biznesowa/RejestracjaILogowanie/SignOut.cpp
+++ Logika Biznesowa/RejestracjaILogowanie/SignOut.cpp
@@ -1,59 +1,59 @@
+////
+//// Created by piotr on 29.08.2023.
+////
 //
-// Created by piotr on 29.08.2023.
+//#include "SignOut.h"
+//#include <iostream>
+//#include "../main.h"
 //
-
-#include "SignOut.h"
-#include <iostream>
-#include "../main.h"
-
-using namespace std;
-
-bool SignOut::signout(){
-    string name,surname,sex,email,username,password,rPassword;
-    int day,month,year;
-    cout<<"Imie: ";
-    cin>>name;
-    cout<<"\nNazwisko: ";
-    cin>>surname;
-    cout<<"\nPlec: ";
-    do{
-        cin>>sex;
-    }
-    while(!isSexCorrect(sex));
-
-    cout<<"\nEmail: ";
-    do{
-        cin>>email;
-    }
-    while(!isEmailCorrect(email));
-
-    cout<<"Data urodzenia:";
-    do{
-        cout<<"\nDzien: ";
-        cin>>day;
-        cout<<"\nMiesiac: ";
-        cin>>month;
-        cout<<"\nRok: ";
-        cin>>year;
-    }
-    while(!isDataCorrect(day,month,year));
-
-    cout<<"\nNazwa uzytkownika: ";
-    cin>>username;
-    cout<<"\nHaslo: ";
-    cin>>password;
-    cout<<"\nPowtorz haslo: ";
-    do{
-        cin>>rPassword;
-    }
-    while(isCorrectRPassword(password,rPassword));
-
-
-
-
-
-
-
-
-}
-
+//using namespace std;
+//
+//bool SignOut::signout(){
+//    string name,surname,sex,email,username,password,rPassword;
+//    int day,month,year;
+//    cout<<"Imie: ";
+//    cin>>name;
+//    cout<<"\nNazwisko: ";
+//    cin>>surname;
+//    cout<<"\nPlec: ";
+//    do{
+//        cin>>sex;
+//    }
+//    while(!isSexCorrect(sex));
+//
+//    cout<<"\nEmail: ";
+//    do{
+//        cin>>email;
+//    }
+//    while(!isEmailCorrect(email));
+//
+//    cout<<"Data urodzenia:";
+//    do{
+//        cout<<"\nDzien: ";
+//        cin>>day;
+//        cout<<"\nMiesiac: ";
+//        cin>>month;
+//        cout<<"\nRok: ";
+//        cin>>year;
+//    }
+//    while(!isDataCorrect(day,month,year));
+//
+//    cout<<"\nNazwa uzytkownika: ";
+//    cin>>username;
+//    cout<<"\nHaslo: ";
+//    cin>>password;
+//    cout<<"\nPowtorz haslo: ";
+//    do{
+//        cin>>rPassword;
+//    }
+//    while(isCorrectRPassword(password,rPassword));
+//
+//
+//
+//
+//
+//
+//
+//
+//}
+//
--- Logika Biznesowa/RejestracjaILogowanie/SignOut.h
+++ Logika Biznesowa/RejestracjaILogowanie/SignOut.h
@@ -1,24 +1,24 @@
+////
+//// Created by piotr on 29.08.2023.
+////
 //
-// Created by piotr on 29.08.2023.
+//#ifndef PROJECTDS_SIGNOUT_H
+//#define PROJECTDS_SIGNOUT_H
 //
-
-#ifndef PROJECTDS_SIGNOUT_H
-#define PROJECTDS_SIGNOUT_H
-
-#include "../WirtualneByty/User.h"
-#include <string>
-
-
-class SignOut {
-public:
-    bool signout();
-    bool isEmailCorrect(string email);
-    bool isSexCorrect(string sex);
-    bool isDataCorrect(int day, int month, int year);
-    bool isCorrectRPassword(string password,string rPassword);
-
-
-};
-
-
-#endif //PROJECTDS_SIGNOUT_H
+//#include "../WirtualneByty/User.h"
+//#include <string>
+//
+//
+//class SignOut {
+//public:
+//    bool signout();
+//    bool isEmailCorrect(string email);
+//    bool isSexCorrect(string sex);
+//    bool isDataCorrect(int day, int month, int year);
+//    bool isCorrectRPassword(string password,string rPassword);
+//
+//
+//};
+//
+//
+//#endif //PROJECTDS_SIGNOUT_H
